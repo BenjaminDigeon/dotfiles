3a73eee9416ec03450a1d45b16d10d73aff7f262
Benjamin Digeon <BenjaminDigeon@gmail.com>
2018-05-03 14:14:53 +0200

    Add a script for symlink dotfiles

---
+++ configure_dotfiles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Symlink each dotfile in the home directory
+# TODO : Backup existing dotfile first
+
+for name in .{bash_prompt,exports,aliases,functions,bashrc,bash_profile}; do
+  source="$PWD/$name"
+  target="$HOME/$name"
+  echo "-----> Symlinking $source to $target"
+  ln -s "$source" "$target"
+done
