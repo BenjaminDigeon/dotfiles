abdbd9829bb6ac124b8c41c633eaa246958af3cf
Benjamin Digeon <BenjaminDigeon@gmail.com>
2018-08-14 18:22:47 +0200

    Upgrade the spotify applescript for not always launching the spotify app when running

--- ubersicht/Sidebar.widget/Playbox.widget/spotify-info.sh
+++ ubersicht/Sidebar.widget/Playbox.widget/spotify-info.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-trackInfo=`osascript <<<'tell application "Spotify"
-        set theTrack to current track
-        set theArtist to artist of theTrack
-        set trackName to name of theTrack
-        set albumName to album of theTrack
-        set artworkUrl to artwork url of theTrack
-        set playerState to player state
-        set percentOfSong to player position / ((duration of current track) / 1000)
-        return  trackName & "|" & theArtist & "|" & albumName & "|" & artworkUrl & "|" & playerState & "|" & percentOfSong & "|"
-    end tell'`;
+trackInfo=`osascript <<< '
+if application "Spotify" is running then
+  tell application "Spotify"
+    set theTrack to current track
+    set theArtist to artist of theTrack
+    set trackName to name of theTrack
+    set albumName to album of theTrack
+    set artworkUrl to artwork url of theTrack
+    set playerState to player state
+    set percentOfSong to player position / ((duration of current track) / 1000)
+  end tell
+  return  trackName & "|" & theArtist & "|" & albumName & "|" & artworkUrl & "|" & playerState & "|" & percentOfSong & "|"
+else
+  return  "" & "|" & "" & "|" & "" & "|" & "" & "|" & "stop" & "|" & "" & "|"
+end if'`;
 
 echo $trackInfo;
